b328849fc1133183aafba25cadd5ee0c63b72476
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-07-11 19:53:29 -0300

    Abertura e Leitura do arquivo de log.

---
+++ ExConjunto01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExConjunto01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\Cursos\CSharp\ExConjunto01\ExConjunto01\Files\input.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
