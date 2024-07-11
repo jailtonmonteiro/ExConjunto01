6ee7b784f4060ff0a8756c31ecc8ba38b4394cc1
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-07-11 20:34:00 -0300

    Implementação da contagem de nomes dos usuários registrados no log.

--- ExConjunto01/Program.cs
+++ ExConjunto01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ExConjunto01.Entities;
 
 namespace ExConjunto01
 {
@@ -6,6 +7,10 @@ namespace ExConjunto01
     {
         static void Main(string[] args)
         {
+
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            //Console.Write("Digite o caminho: ");
             string path = @"E:\Cursos\CSharp\ExConjunto01\ExConjunto01\Files\input.txt";
 
             try
@@ -14,10 +19,13 @@ namespace ExConjunto01
                 {
                     while (!sr.EndOfStream)
                     {
-                        string line = sr.ReadLine();
-                        Console.WriteLine(line);
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord(name, instant));
                     }
                 }
+                Console.Write($"Total de usuários: {set.Count}");
 
             }
             catch (IOException e)
